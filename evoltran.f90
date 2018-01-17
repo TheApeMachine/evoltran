34fa4118cf586822454705b26d813816e53db496
Daniel Owen van Dommelen <theapemachine@gmail.com>
2018-01-17 15:14:17 +0000

    Start tokenizing

--- evoltran.f90
+++ evoltran.f90
@@ -21,6 +21,25 @@ subroutine lexer(in)
   implicit none
 
   character(255) :: in
-  print *, in
+  character(5) :: word(5)
+  integer :: pos1 = 1, pos2, n = 0, i
+
+  do
+    pos2 = index(in(pos1:), " ")
+
+    if (pos2 == 0) then
+      n = n + 1
+      word(n) = in(pos1:)
+      exit
+    end if
+
+    n = n + 1
+    word(n) = in(pos1:pos1+pos2-2)
+    pos1 = pos2 + pos1
+  end do
+
+  do i = 1, n
+    write(*, "(2A)", advance="no") trim(word(i)), "."
+  end do
 
 end subroutine
